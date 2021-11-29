96ad97158f6b65838b4f23a7afc8f4fb9d44767f
Honza Sedloň <jansedlon@icloud.com>
2021-11-29 23:51:43 +0100

    Update entry.client.tsx

--- examples/remix-with-typescript/app/entry.client.tsx
+++ examples/remix-with-typescript/app/entry.client.tsx
@@ -1,4 +1,13 @@
 import { hydrate } from 'react-dom';
 import { RemixBrowser } from 'remix';
+import createEmotionCache from './createEmotionCache';
+import { CacheProvider } from '@emotion/react';
 
-hydrate(<RemixBrowser />, document);
+const clientSideEmotionCache = createEmotionCache();
+
+// For emotion to correctly pickup styles, the CacheProvider need to be in entry.client.tsx
+hydrate(
+  <CacheProvider value={clientSideEmotionCache}>
+    <RemixBrowser />
+  </CacheProvider>
+, document);
